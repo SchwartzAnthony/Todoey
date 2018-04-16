5655d2eb2f7b13ba6a1a02aa20e5f01f909716d0
Anthony Schwartz <anthonyschwartz@anthonys-imac.local>
2018-04-16 09:33:12 -0500

    Convert to MVC Design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Anthony Schwartz on 4/13/18.
+//  Copyright © 2018 Anthony Schwartz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
